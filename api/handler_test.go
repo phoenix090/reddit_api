d45574200172e40708cc095c3fc29732d286b587
Hamse Hashi <hamseah@stud.ntnu.no>
2018-11-15 22:30:20 +0100

    merge

---
+++ api/handler_test.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"testing"
+)
+
+// Tests that the redirect function works properly
+func TestRedirect(t *testing.T) {
+
+}
+
+//
+func TestInfoHandler(t *testing.T) {
+
+}
