cdb8684f9c8b1e3400a45c2461895fb0b52305a7
Hamse Hashi <hamseah@stud.ntnu.no>
2018-11-19 14:12:19 +0100

    more bot functionality

--- bot/bot.go
+++ bot/bot.go
@@ -4,18 +4,24 @@ import (
 	"context"
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"reddit_api/api"
 	"reddit_api/model"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/shomali11/slacker"
 )
 
+// The slacktoken for the bot
 var bot = slacker.NewClient(os.Getenv("SLACKTOKEN"))
 
+// Makes use of our own reddit api end points to get information
+// The function parameter @f is a placeholder for our own api handlers.
 func makeReq(url string, meth string, f func(http.ResponseWriter, *http.Request)) *httptest.ResponseRecorder {
 	req, err := http.NewRequest(meth, url, nil)
 	if err != nil {
@@ -28,15 +34,23 @@ func makeReq(url string, meth string, f func(http.ResponseWriter, *http.Request)
 	return writer
 }
 
-func handleMe(request slacker.Request, response slacker.ResponseWriter) {
+// Handles when someone types "me" in the bot chat, replies with the name of the current user thats logged in with oauth
+func handleMe(word string, response slacker.ResponseWriter) {
 
 	w := makeReq("http://localhost:8080/reddit/api/me/", "GET", api.GetUserInfo)
 	var user model.User
 	json.NewDecoder(w.Body).Decode(&user)
-
-	response.Reply("You are " + user.Name + " :)")
+	words := []string{"me", "whoami", "whats my name", "guess my name", "can you guess my name", "who am i", "say my name"}
+	for _, v := range words {
+		if v == word {
+			response.Reply("You are " + user.Name + " :)")
+			return
+		}
+	}
+	response.Reply("I dont understand, can you ask simpler questions? I am after all a simple bot :(")
 }
 
+// Handles when someonetype "friends", the bot replies with how many friends the current user has
 func handleGetFriends(request slacker.Request, response slacker.ResponseWriter) {
 	//GetFriends
 	w := makeReq("http://localhost:8080/reddit/api/me/friends/", "GET", api.GetFriends)
@@ -55,11 +69,12 @@ func handleGetFriends(request slacker.Request, response slacker.ResponseWriter) 
 
 }
 
-//TODO comment
-func test(mess string, desc string, handler func(slacker.Request, slacker.ResponseWriter)) {
+// Handles input from the user and sends them to other handlers to get reply
+func handleInput(mess string, desc string, handler func(slacker.Request, slacker.ResponseWriter)) {
 	bot.Command(mess, desc, handler)
 }
 
+// Replies with the amount of karma a user got on reddit, the param name represent the reddit username
 func handleKarma(request slacker.Request, response slacker.ResponseWriter) {
 
 	name := request.Param("name")
@@ -72,12 +87,35 @@ func handleKarma(request slacker.Request, response slacker.ResponseWriter) {
 	response.Reply("the user, " + name + " has " + tKarma + " karma")
 }
 
+// Replies with the prefered language the user selected on reddit
 func handlePrefs(request slacker.Request, response slacker.ResponseWriter) {
 	w := makeReq("http://localhost:8080/reddit/api/me/prefs/", "GET", api.GetPrefs)
 	var prefs model.Preferences
 	json.NewDecoder(w.Body).Decode(&prefs)
 
 	response.Reply("Your preference language is: " + prefs.Language)
+}
+
+func generic(request slacker.Request, response slacker.ResponseWriter) {
+
+	// Handling all the types of greetings
+	word := request.Param("generic")
+	word = strings.ToLower(word)
+	greetings := []string{"hi", "hello", "hey", "hei", "greeting", "hola", "yo", "wassup", "sup", ":wave:"}
+	answers := []string{"Salutations!", "Greetings!", "Hey there!", "Hello!", "Welcome back!", "Hola!", "Hi! :)", ":wave:"}
+	sec := time.Now().Second()
+
+	// Making greetings random
+	rand.Seed(int64(sec))
+	for _, v := range greetings {
+		if strings.HasPrefix(word, v) {
+			response.Reply(answers[rand.Intn(len(answers))])
+			return
+		}
+	}
+
+	// handles all types of me, like whoami, guess my name etc.
+	handleMe(word, response)
 }
 
 // StartBot initiates the bot to start listening to requests
@@ -90,10 +128,12 @@ func StartBot() {
 		log.Println(err)
 	})
 
+	// Default answer when there is no handlers for that spesific input from user to the bot
 	bot.DefaultCommand(func(request slacker.Request, response slacker.ResponseWriter) {
 		response.Reply("Say what?")
 	})
 
+	// Help output to the user when they type "help"
 	bot.Help(func(request slacker.Request, response slacker.ResponseWriter) {
 		response.Reply(`You can type following to talk to me: 
 						- me [i will guess your name :)]
@@ -101,11 +141,15 @@ func StartBot() {
 						- friends [i will give you a list of your friends]
 						- karma <username> on reddit [I will get how many karma's the user has :P]`)
 	})
-	test("me", "", handleMe)
-	test("prefs", "Gets the users prefs", handlePrefs)
-	test("friends", "", handleGetFriends)
-	test("karma <name>", "Find an users karma", handleKarma)
-	// TODO test("posts <name> <sortby>", "Gets posts", handleKarma)
+
+	// Handlers for spesific keywords
+	handleInput("prefs", "Gets the users prefs", handlePrefs)
+	handleInput("friends", "", handleGetFriends)
+	handleInput("karma <name>", "Find an users karma", handleKarma)
+	// For generic requests
+	bot.Command("<generic>", "handles greetings and other stuff like whoami and similar", generic)
+
+	// TODO handleInput("posts <name> <sortby>", "Gets posts", handleKarma)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
